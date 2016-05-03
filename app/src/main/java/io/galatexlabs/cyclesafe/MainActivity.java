aac943df66dbed489b7a50658e425d08e42ad735
Jeremy <jlc304@txstate.edu>
2016-05-02 19:04:41 -0500

    Implemented Login Activity. Added temp logout button on main activity.

--- app/src/main/java/io/galatexlabs/cyclesafe/MainActivity.java
+++ app/src/main/java/io/galatexlabs/cyclesafe/MainActivity.java
@@ -2,18 +2,43 @@ package io.galatexlabs.cyclesafe;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import helper.SQLiteHandler;
+import helper.SessionManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogout;
+    
+    private SQLiteHandler db;
+    private SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
 
         // Custom font functions //
 
@@ -32,11 +57,30 @@ public class MainActivity extends AppCompatActivity {
         Typeface rbface=Typeface.createFromAsset(getAssets(),"fonts/AmaticSC-Bold.ttf");
         rbview.setTypeface(rbface);
     }
-    /** Called when the user clicks the Send button */
+    /** Called when the user clicks the Register button */
     public void onClickRegister(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
 
+    public void onClickLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 
+
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     * */
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
